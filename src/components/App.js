148f13bf240de09b57869763d38acde16ff8397b
elorenzen <eric.lorenzen@gmail.com>
2019-06-04 20:19:14 -0600

    Add basic navbar and Stepbar from imported Element-UI components

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import { Steps, Menu } from 'element-react/next';
 
-const App = () => {
-    return <div>App component</div>
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <Menu defaultActive="1" className="el-menu-demo" mode="horizontal">
+                    <Steps space={600} active={1} finishStatus="success">
+                        <Steps.Step title="Done"></Steps.Step>
+                        <Steps.Step title="Processing"></Steps.Step>
+                        <Steps.Step title="Step 3"></Steps.Step>
+                    </Steps>
+                </Menu>
+            </div>   
+        )
+    }
 }
 
 export default App;
