7e8c8788688014efe7d69379281128280d3dc6b6
elorenzen <eric.lorenzen@gmail.com>
2019-06-07 11:11:59 -0600

    Change Element-UI steps

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,37 @@
 import React from 'react';
-import { Steps, Menu } from 'element-react/next';
+import { Steps, Menu, Button } from 'element-react/next';
 
 class App extends React.Component {
-    render() {
+    constructor(props) {
+        super(props);
+      
+        this.state = {
+          active: 0
+        };
+      }
+      
+      next() {
+        let active = this.state.active + 1;
+        if (active > 3) {
+          active = 0;
+        }
+        this.setState({ active });
+      }
+      
+      render() {
         return (
-            <div>
-                <Menu defaultActive="1" className="el-menu-demo" mode="horizontal">
-                    <Steps space={600} active={1} finishStatus="success">
-                        <Steps.Step title="Done"></Steps.Step>
-                        <Steps.Step title="Processing"></Steps.Step>
-                        <Steps.Step title="Step 3"></Steps.Step>
-                    </Steps>
-                </Menu>
-            </div>   
+          <div>
+            <Menu defaultActive="1" className="el-menu-demo" mode="horizontal">
+                <Steps space={200} active={this.state.active} finishStatus="success">
+                    <Steps.Step title="Company & Contact"></Steps.Step>
+                    <Steps.Step title="Options & Services"></Steps.Step>
+                    <Steps.Step title="Review & Pay"></Steps.Step>
+                </Steps>
+            </Menu>
+            <Button onClick={() => this.next()}>Next step</Button>
+          </div>
         )
-    }
+      }
 }
 
 export default App;
