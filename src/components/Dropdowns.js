5937faeddde25465783dd9311337a5a2f9d6a2a0
elorenzen <eric.lorenzen@gmail.com>
2019-06-07 12:38:20 -0600

    Add form elements for Company

--- src/components/Dropdowns.js
+++ src/components/Dropdowns.js
@@ -1,14 +1,136 @@
 import React from 'react';
-import { Collapse } from 'element-react/next';
+import { Collapse, Input, Button, Form } from 'element-react/next';
 
 class Dropdowns extends React.Component {
+    constructor(props) {
+        super(props);
+  
+        this.state = {
+            form: {
+                altnames: [{
+                key: 1,
+                value: ''
+                }]
+            }
+        };
+    }
+  
+    handleSubmit(e) {
+        e.preventDefault();
+  
+        this.refs.form.validate((valid) => {
+            if (valid) {
+                alert('submit!');
+            } else {
+                console.log('error submit!!');
+                return false;
+            }
+        });
+    }
+  
+    removeAltName(item, e) {
+        var index = this.state.form.altnames.indexOf(item);
+  
+        if (index !== -1) {
+            this.state.form.altnames.splice(index, 1);
+            this.forceUpdate();
+        }
+  
+        e.preventDefault();
+    }
+  
+    addAltName(e) {
+        e.preventDefault();
+  
+        this.state.form.altnames.push({
+            key: this.state.form.altnames.length,
+            value: ''
+        });
+  
+        this.forceUpdate();
+    }
+  
+    onEmailChange(value) {
+        this.setState({
+            form: Object.assign({}, this.state.form, { email: value })
+        });
+    }
+  
+    onAltNameChange(index, value) {
+        this.state.form.altnames[index].value = value;
+        this.forceUpdate();
+    }
+
     render() {
         const activeName = "1";
         return (
             <Collapse value={activeName} accordion>
                 <Collapse.Item title="Company" name="1">
-                    <div>Consistent with real life: in line with the process and logic of real life, and comply with languages and habits that the users are used to;</div>
-                    <div>Consistent within interface: all elements should be consistent, such as: design style, icons and texts, position of elements, etc.</div>
+                    <Form ref="form" model={this.state.form} rules={this.state.rules} labelWidth="100" className="demo-dynamic"> 
+                        <Form.Item>
+                            <div>State</div>
+                            <Input disabled placeholder="Delaware" value="Delaware" />
+                        </Form.Item>
+                        <Form.Item>
+                            <div>Type</div>
+                            <Input disabled placeholder="LLC" value="LLC" />
+                        </Form.Item>
+                        <Form.Item
+                            label={`Company name`}
+                            rules={{
+                                type: 'object', required: true,
+                                fields: {
+                                    value: { 
+                                        required: true, 
+                                        message: 'You must name your LLC', 
+                                        trigger: 'blur' 
+                                    }
+                                }
+                            }}>
+                            <Input />
+                        </Form.Item>
+                        {
+                            this.state.form.altnames.map((altname, index) => {
+                                return (
+                                    <Form.Item
+                                        key={index}
+                                        label={`Alternative name ${index}`}
+                                        prop={`altnames:${index}`}
+                                        rules={{
+                                            type: 'object', required: true,
+                                            fields: {
+                                                value: { 
+                                                    required: true, 
+                                                    message: 'You must add at least one alternate name', 
+                                                    trigger: 'blur' 
+                                                }
+                                            }
+                                        }}
+                                    >
+                                        <Input 
+                                            value={altname.value} 
+                                            onChange={this.onAltNameChange.bind(this, index)}>
+                                        </Input>
+                                        <Button 
+                                            onClick={this.removeAltName.bind(this, altname)}>
+                                            Delete
+                                        </Button>
+                                    </Form.Item>
+                                )
+                            })
+                        }
+                        <Form.Item>
+                            <Button 
+                                type="primary" 
+                                onClick={this.handleSubmit.bind(this)}>
+                                Submit
+                            </Button>
+                            <Button 
+                                onClick={this.addAltName.bind(this)}>
+                                New name
+                            </Button>
+                        </Form.Item>
+                    </Form>
                 </Collapse.Item>
                 <Collapse.Item title="Contact" name="2">
                     <div>Operation feedback: enable the users to clearly perceive their operations by style updates and interactive effects;</div>
