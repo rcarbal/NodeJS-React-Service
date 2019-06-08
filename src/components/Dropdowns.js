d113126affe13413684eae8e80c9f5a6a9433714
elorenzen <eric.lorenzen@gmail.com>
2019-06-07 19:53:24 -0600

    Add city, state, zip and country form elements

--- src/components/Dropdowns.js
+++ src/components/Dropdowns.js
@@ -16,10 +16,169 @@ class Dropdowns extends React.Component {
             labelPosition: 'top',
             form: {
                 altnames: [{
-                key: 1,
-                value: ''
+                    key: 1,
+                    value: ''
                 }]
-            }
+            },
+            states: [{
+              value: 'AL',
+              label: 'Alabama'
+            }, {
+              value: 'AK',
+              label: 'Alaska'
+            }, {
+              value: 'AZ',
+              label: 'Arizona'
+            }, {
+              value: 'AR',
+              label: 'Arkansas'
+            }, {
+              value: 'CA',
+              label: 'California'
+            }, {
+              value: 'CO',
+              label: 'Colorado'
+            }, {
+              value: 'CT',
+              label: 'Connecticut'
+            }, {
+              value: 'DE',
+              label: 'Delaware'
+            }, {
+              value: 'DC',
+              label: 'District of Columbia'
+            }, {
+              value: 'FL',
+              label: 'Florida'
+            }, {
+              value: 'GA',
+              label: 'Georgia'
+            }, {
+              value: 'HI',
+              label: 'Hawaii'
+            }, {
+              value: 'ID',
+              label: 'Idaho'
+            }, {
+              value: 'IN',
+              label: 'Illinois'
+            }, {
+              value: 'IN',
+              label: 'Indiana'
+            }, {
+              value: 'IA',
+              label: 'Iowa'
+            }, {
+              value: 'KS',
+              label: 'Kansas'
+            }, {
+              value: 'KY',
+              label: 'Kentucky'
+            }, {
+              value: 'LA',
+              label: 'Louisiana'
+            }, {
+              value: 'ME',
+              label: 'Maine'
+            }, {
+              value: 'MD',
+              label: 'Maryland'
+            }, {
+              value: 'MA',
+              label: 'Massachusetts'
+            }, {
+              value: 'MI',
+              label: 'Michigan'
+            }, {
+              value: 'MN',
+              label: 'Minnesota'
+            }, {
+              value: 'MS',
+              label: 'Mississippi'
+            }, {
+              value: 'MO',
+              label: 'Missouri'
+            }, {
+              value: 'MT',
+              label: 'Montana'
+            }, {
+              value: 'NE',
+              label: 'Nebraska'
+            }, {
+              value: 'NV',
+              label: 'Nevada'
+            }, {
+              value: 'NH',
+              label: 'New Hampshire'
+            }, {
+              value: 'NJ',
+              label: 'New Jersey'
+            }, {
+              value: 'NM',
+              label: 'New Mexico'
+            }, {
+              value: 'NY',
+              label: 'New York'
+            }, {
+              value: 'NC',
+              label: 'North Carolina'
+            }, {
+              value: 'ND',
+              label: 'North Dakota'
+            }, {
+              value: 'OH',
+              label: 'Ohio'
+            }, {
+              value: 'OK',
+              label: 'Oklahoma'
+            }, {
+              value: 'OR',
+              label: 'Oregon'
+            }, {
+              value: 'PA',
+              label: 'Pennsylvania'
+            }, {
+              value: 'RI',
+              label: 'Rhode Island'
+            }, {
+              value: 'SC',
+              label: 'South Carolina'
+            }, {
+              value: 'SD',
+              label: 'South Dakota'
+            }, {
+              value: 'TN',
+              label: 'Tennessee'
+            }, {
+              value: 'TX',
+              label: 'Texas'
+            }, {
+              value: 'UT',
+              label: 'Utah'
+            }, {
+              value: 'VT',
+              label: 'Vermont'
+            }, {
+              value: 'VA',
+              label: 'Virginia'
+            }, {
+              value: 'WA',
+              label: 'Washington'
+            }, {
+              value: 'WV',
+              label: 'West Virginia'
+            }, {
+              value: 'WI',
+              label: 'Wisconsin'
+            }, {
+              value: 'WY',
+              label: 'Wyoming'
+            }],
+            country: [{
+              value: 'USA',
+              label: 'United States of America'
+            }],
+            value: ''
         };
     }
   
@@ -168,38 +327,86 @@ class Dropdowns extends React.Component {
                                 </Form.Item>
                             </Layout.Col>
                             <Layout.Col span='12'>
-                                <Input placeholder='Last name' gutter="20" />
+                                <Form.Item>
+                                    <Input placeholder='Last name' gutter="20" />
+                                </Form.Item>                                     
                             </Layout.Col>
                         </Layout.Row>
                         <Layout.Row>
                             <Layout.Col span='12'>
-                                <Input type='email' placeholder="Email address" append={
-                                    <Select value=''> {
-                                        [
-                                            ".com",
-                                            ".net",
-                                            ".edu",
-                                            ".gov"
-                                        ].map((item, index) => 
-                                            <Select.Option 
-                                                key={index} 
-                                                label={item} 
-                                                value={index} 
-                                            />
-                                        )
-                                    } </Select>
-                                } />
+                                <Form.Item>
+                                    <Input type='email' placeholder="Email address" append={
+                                        <Select value=''> {
+                                            [
+                                                ".com",
+                                                ".net",
+                                                ".edu",
+                                                ".gov"
+                                            ].map((item, index) => 
+                                                <Select.Option 
+                                                    key={index} 
+                                                    label={item} 
+                                                    value={index} 
+                                                />
+                                            )
+                                        } </Select>
+                                    } />
+                                </Form.Item>           
                             </Layout.Col> 
                             <Layout.Col span='12'>
-                                <Input type='number' placeholder="Phone number" />
+                                <Form.Item>
+                                    <Input type='number' placeholder="Phone number" />
+                                </Form.Item>                      
                             </Layout.Col>               
                         </Layout.Row>
                         <Layout.Row>
                             <Layout.Col span='12'>
-                                <Input type='text' placeholder="Street Address" />
+                                <Form.Item>
+                                    <Input type='text' placeholder="Street Address" />
+                                </Form.Item>
                             </Layout.Col>
                             <Layout.Col span='12'>
-                                <Input type='text' placeholder="Street Address continued" />
+                                <Form.Item>
+                                    <Input type='text' placeholder="Street Address continued" />
+                                </Form.Item>
+                            </Layout.Col>
+                        </Layout.Row>
+                        <Layout.Row>
+                            <Layout.Col span='11'>
+                                <Form.Item>
+                                    <Input type='text' placeholder="City" />
+                                </Form.Item>
+                            </Layout.Col>
+                            <Layout.Col span='5'>
+                                <Form.Item>
+                                    <Select value={this.state.value}> {
+                                        this.state.states.map(el => {
+                                            return  <Select.Option 
+                                                        key={el.value} 
+                                                        label={el.label} 
+                                                        value={el.value} 
+                                                    />
+                                        })
+                                    } </Select>
+                                </Form.Item>
+                            </Layout.Col>
+                            <Layout.Col span="8">
+                                <Form.Item>
+                                    <Input placeholder="Zip Code" />
+                                </Form.Item>
+                            </Layout.Col>
+                            <Layout.Col span='24'>
+                                <Form.Item>
+                                    <Select value={ this.state.value }> {
+                                        this.state.country.map(item => {
+                                            return  <Select.Option
+                                                        key={item.value}
+                                                        label={item.label}
+                                                        value={item.label}
+                                                    />
+                                        })
+                                    } </Select>
+                                </Form.Item>
                             </Layout.Col>
                         </Layout.Row>
                     </Form>
