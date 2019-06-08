d28a35678dd0a103664673892d23aef1f6fd6ca3
elorenzen <eric.lorenzen@gmail.com>
2019-06-08 10:51:53 -0600

    Add redux-form form reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,7 @@
 import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
 
 export default combineReducers({
-    dummy: 'replace this'
+    // must use 'form' to work with redux-form
+    form: formReducer
 });
