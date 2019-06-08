d40edf910ac27f29ae8479361e391a703a142835
elorenzen <eric.lorenzen@gmail.com>
2019-06-07 19:53:56 -0600

    Import language locale

--- src/index.js
+++ src/index.js
@@ -2,7 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'element-theme-default';
 
+import { i18n } from 'element-react'
+import locale from 'element-react/src/locale/lang/en'
+
 import App from './components/App';
+
+i18n.use(locale);
 
 ReactDOM.render(
     <App />, 
