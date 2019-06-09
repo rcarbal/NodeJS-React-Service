7ee488037c4586d86986565482dbad6320ac3959
elorenzen <eric.lorenzen@gmail.com>
2019-06-08 18:42:51 -0600

    Move menu JSX to FormContainer

--- src/components/App.js
+++ src/components/App.js
@@ -1,40 +1,19 @@
 import React from 'react';
 import { Steps, Menu, Button } from 'element-react/next';
 
-import Dropdowns from './Dropdowns';
+import FormContainer from './FormContainer';
 
 class App extends React.Component { 
     // Function overriding React.Component constructor() 
     constructor(props) {
         // Calls code in parent(React.Component)
         super(props);
-      
-        this.state = {
-            active: 0
-        };
     }
-      
-    next() {
-        let active = this.state.active + 1;
-        if (active > 3) {
-          active = 0;
-        }
-        this.setState({ active });
-    }
+        
     render() {
         return (
             <div>
-                <Menu defaultActive="1" className="el-menu-demo" mode="horizontal">
-                    <Steps space={500} active={this.state.active} style={{marginLeft: '22%', padding: '10px'}} finishStatus="success">
-                        <Steps.Step title="Company & Contact"></Steps.Step>
-                        <Steps.Step title="Options & Services"></Steps.Step>
-                        <Steps.Step title="Review & Pay"></Steps.Step>
-                    </Steps>
-                </Menu>
-                <Dropdowns></Dropdowns>
-                <Button onClick={() => this.next()}>
-                    Next step
-                </Button>        
+                <FormContainer></FormContainer>  
             </div>
         )
     }
