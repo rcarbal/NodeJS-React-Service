1f8e83c52bcf31929098d54e4ae9a6ce0eed90ad
elorenzen <eric.lorenzen@gmail.com>
2019-06-12 10:38:52 -0600

    Add basic Radio buttons for choosing formation package

--- src/components/OptionsContainer.js
+++ src/components/OptionsContainer.js
@@ -1,11 +1,41 @@
 import React from 'react';
 
-const OptionsContainer = () => {
-    return (
-        <div>
-            <h1>Options and Services</h1>
-        </div>
-    )
+import { Form, Radio, Layout } from 'element-react/next';
+
+class OptionsContainer extends React.Component {
+
+    state = {
+        package: {
+            value: '',
+            price: ''
+        }
+    }
+
+    render(){
+        return (
+            <div>
+                <Form>
+                    <h3>Package</h3>
+                    <Form.Item> 
+                        <Radio.Group value={this.state.package.value} price={this.state.package.price}>
+                            <Radio value='SmoothLegal' price='399'>SmoothLegal Formation</Radio>
+                            <Radio value='Complete' price='298'>Complete Formation</Radio>
+                            <Radio value='Basic' price='189'>Basic Formation</Radio>
+                        </Radio.Group>
+                    </Form.Item>
+                    <Form.Item>
+                        <h3>Popular Services</h3>
+                    </Form.Item>
+                    <Form.Item>
+                        <h3>Delivery Options</h3>
+                    </Form.Item>
+                    <Form.Item>
+                        <h3>Special Requests</h3>
+                    </Form.Item>
+                </Form>
+            </div>
+        )
+    }
 }
 
 export default OptionsContainer;
