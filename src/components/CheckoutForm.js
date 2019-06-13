f1c4b4b8c4797ce9f82acda26a6467064f625bf0
elorenzen <eric.lorenzen@gmail.com>
2019-06-12 19:18:24 -0600

    Add components for Stripe payment elements

---
+++ src/components/CheckoutForm.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class CheckoutForm extends React.Component {
+    render() {
+        return (
+            <div>Checkout Form</div>
+        )
+    }
+};
+
+export default CheckoutForm;
--- src/components/PayContainer.js
+++ src/components/PayContainer.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import { StripeProvider } from 'react-stripe-elements';
+
+import CheckoutContainer from './CheckoutContainer';
 
 class PayContainer extends React.Component {
     render() {
@@ -6,6 +9,9 @@ class PayContainer extends React.Component {
             <div>
                 <h3>Review</h3>
                 <h3>Pay</h3>
+                <StripeProvider apiKey='pk_test_LDIkUEQ3WzsMPUFHtVRRpq6s00jN1E6PVE'>
+                    <CheckoutContainer />
+                </StripeProvider>
             </div>
         )
     }
