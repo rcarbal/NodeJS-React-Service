ab7b1a8f59b8f39a59f76857aefc5c8a37570d59
elorenzen <eric.lorenzen@gmail.com>
2019-06-13 22:35:37 -0600

    Add code containing basic Checkout Form Container setup from Stripe docs

--- src/components/CheckoutContainer.js
+++ src/components/CheckoutContainer.js
@@ -2,15 +2,42 @@ import React from 'react';
 import { Elements } from 'react-stripe-elements';
 
 import CheckoutForm from './CheckoutForm';
+import PaymentRequestForm from './PaymentRequestForm';
 
 class CheckoutContainer extends React.Component {
-    render() {
+    constructor() {
+        super();
+        this.state = {
+          elementFontSize: window.innerWidth < 450 ? '14px' : '18px',
+        };
+        window.addEventListener('resize', () => {
+          if (window.innerWidth < 450 && this.state.elementFontSize !== '14px') {
+            this.setState({elementFontSize: '14px'});
+          } else if (
+            window.innerWidth >= 450 &&
+            this.state.elementFontSize !== '18px'
+          ) {
+            this.setState({elementFontSize: '18px'});
+          }
+        });
+      }
+    
+      render() {
+        const {elementFontSize} = this.state;
         return (
+          <div className="Checkout">
+            <h1>Available Elements</h1>
             <Elements>
-                <CheckoutForm />
+              <CheckoutForm fontSize={elementFontSize} />
             </Elements>
-        )
-    }
+            {/*
+            <Elements>
+              <PaymentRequestForm />
+            </Elements>
+            */}
+          </div>
+        );
+      }
 };
 
 export default CheckoutContainer;
