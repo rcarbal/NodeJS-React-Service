cdbdff78acba9b0548c8b8a3b09e3b9b210e4ea3
elorenzen <eric.lorenzen@gmail.com>
2019-06-17 06:50:39 -0600

    Delete reducers directory-- not using Redux

--- client/src/reducers/index.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from 'redux';
-import { reducer as formReducer } from 'redux-form';
-
-export default combineReducers({
-    // must use 'form' to work with redux-form
-    form: formReducer
-});
