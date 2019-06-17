949c93d5018b73476a6fae94422c3792c113ff43
elorenzen <eric.lorenzen@gmail.com>
2019-06-17 07:01:35 -0600

    Remove all components related to Redux

--- client/src/index.js
+++ client/src/index.js
@@ -1,20 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
 import 'element-theme-default';
 
 import { i18n } from 'element-react'
 import locale from 'element-react/src/locale/lang/en'
 
 import App from './components/App';
-import reducers from './reducers';
+
 
 i18n.use(locale);
 
 ReactDOM.render(
-    <Provider store={createStore(reducers)}>
-        <App />
-    </Provider>, 
+    <App />, 
     document.querySelector('#root')
 );
