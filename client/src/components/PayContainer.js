4597af237a6348d3af774f381631703d9d469c40
elorenzen <eric.lorenzen@gmail.com>
2019-06-21 08:19:18 -0600

    Remove StripeProvider component around CheckoutConainer component

--- client/src/components/PayContainer.js
+++ client/src/components/PayContainer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StripeProvider } from 'react-stripe-elements';
+
 import { Button } from 'element-react/next';
 
 import CheckoutContainer from './CheckoutContainer';
@@ -23,9 +23,7 @@ class PayContainer extends React.Component {
             <div>
                 <h3>Review</h3>
                 <h3>Pay</h3>
-                <StripeProvider apiKey='pk_test_LDIkUEQ3WzsMPUFHtVRRpq6s00jN1E6PVE'>
-                    <CheckoutContainer />
-                </StripeProvider>
+                <CheckoutContainer />
                 <Button onClick={this.onBack.bind(this)}>
                     Back
                 </Button>
