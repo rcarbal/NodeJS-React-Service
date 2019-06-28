0f8a1d440e650609a232e211c90128771aa38c7b
elorenzen <eric.lorenzen@gmail.com>
2019-06-28 00:34:54 -0600

    Delete CheckoutContainer and CheckoutForm files, remove broken/old/commented code

--- client/src/components/PayContainer.js
+++ client/src/components/PayContainer.js
@@ -1,25 +1,8 @@
 import React from 'react';
-
 import { Button } from 'element-react/next';
-
-import CheckoutContainer from './CheckoutContainer';
 import Payments from './Payments';
 
 class PayContainer extends React.Component {
-
-
-    componentDidMount(){
-        console.log("PRINTING PROPS OF PAYCONTAINER =========================")
-        console.log(this.props)
-    }
-
-
-    handleSubmit(event) {
-        event.preventDefault();
-
-        //this.props.finalSubmit(this.state);
-    }
-
     onBack(event) {
         event.preventDefault();
 
@@ -53,9 +36,6 @@ class PayContainer extends React.Component {
                 </div>
                 <Button onClick={this.onBack.bind(this)}>
                     Back
-                </Button>
-                <Button type='submit' onClick={this.handleSubmit.bind(this)}>
-                    Submit
                 </Button>
             </div>
         )
