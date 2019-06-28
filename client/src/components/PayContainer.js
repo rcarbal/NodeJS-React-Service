c1a80b488c953f342c310308101ac7e01110dba9
elorenzen <eric.lorenzen@gmail.com>
2019-06-28 02:32:03 -0600

    Add review info data to bootstrap table

--- client/src/components/PayContainer.js
+++ client/src/components/PayContainer.js
@@ -39,7 +39,6 @@ class PayContainer extends React.Component {
             <div>
                 <h3>Review</h3>
                 <div className='container card'>
-                <div>================</div>
                     <h5>Order for: {this.props.stateOfIncorporation} {this.props.type}</h5>
                     <h4>{this.props.companyName}</h4>
                     <h6>(Alternate name: {this.props.altName})</h6>
@@ -55,17 +54,47 @@ class PayContainer extends React.Component {
                     </p>
                     <div>
                         <strong>Services: </strong>
-                            <p>llcPackage = {llcPackage} - {llcPackagePrice}</p>
-                            <p>certifiedCopies={numCertifiedCopies} X {certifiedCopiesPrice}</p>
-                            <p>certifiedCopiesWApostille={numCertifiedCopiesWApostille} X {certifiedCopiesWApostillePrice}</p>
-                            <p>goodStandingCopies={numGoodStandingCopies} X {goodStandingCopiesPrice}</p>
-                            <p>goodStandingCopiesWApostille={numGoodStandingCopiesWApostille} X {goodStandingCopiesWApostillePrice}</p>
                             <p>servicesList=</p>
                             <p>deliveryOption=</p>
                             <p>paymentTotal={paymentTotal}</p>
                             <p>specialRequests={specialRequests}</p>
+                            <table class="table">
+                                <thead>
+                                    <tr>
+                                        <th scope="col">Item</th>
+                                        <th scope="col">Quantity</th>
+                                        <th scope="col">Price</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>'{llcPackage}' Package</td>
+                                        <td></td>
+                                        <td>${llcPackagePrice}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Certified Copies</td>
+                                        <td>{numCertifiedCopies}</td>
+                                        <td>${certifiedCopiesPrice}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Certified Copies w/ Apostille</td>
+                                        <td>{numCertifiedCopiesWApostille}</td>
+                                        <td>${certifiedCopiesWApostillePrice}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Certificates of Good Standing</td>
+                                        <td>{numGoodStandingCopies}</td>
+                                        <td>${goodStandingCopiesPrice}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Certificates of Good Standing w/ Apostille</td>
+                                        <td>{numGoodStandingCopiesWApostille}</td>
+                                        <td>${goodStandingCopiesWApostillePrice}</td>
+                                    </tr>
+                                </tbody>
+                            </table>
                     </div>
-                <div>================</div>
                 </div>
                 <h3>Pay</h3>
                 <div className='container card'>
