d6a9fbae642bb50b95b82b453bd2272a6e4857bd
elorenzen <eric.lorenzen@gmail.com>
2019-07-06 12:53:45 -0600

    Closes: #17

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Navbar = () => {
     return (
-        <nav class="navbar navbar-light bg-light">
-            <span class="navbar-brand mb-0 h1">SmoothLegal</span>
+        <nav className="navbar navbar-light bg-light">
+            <span className="navbar-brand mb-0 h1">SmoothLegal</span>
         </nav>
     )
 };
