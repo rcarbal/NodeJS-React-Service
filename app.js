4c57c16ab3b3094928a0f2f8cd7a10e8f004ee1d
Ricardo Carballo <rcarbaleq2@gmail.com>
2019-07-12 14:15:59 -0700

    route-comment

--- app.js
+++ app.js
@@ -4,21 +4,19 @@
 
 // const testApiRoutes = require('./routes/devApi'),
     //   paidApiRoutes = require('./routes/prodApi'), 
-  const certRoutes = require('./routes/serverCertsApi'),
-      bodyParser    = require("body-parser"),
-      express       = require('express'),
+  const express = require('express'),
       app           = express();
 
 
-app.set("view engine", "ejs");
-app.use(bodyParser.json()); 
-app.use(bodyParser.urlencoded({extended: true}));
+// app.set("view engine", "ejs");
+// app.use(bodyParser.json()); 
+// app.use(bodyParser.urlencoded({extended: true}));
 
 //app.use(bodyParser.urlencoded({ extended: true }));
 // app.use(bodyParser.text());
 //app.use(bodyParser.json());
 
-app.use(certRoutes);
+// app.use(certRoutes);
 // app.use(testApiRoutes);
 
 // app.use(paidApiRoutes);
@@ -31,6 +29,15 @@ app.use(certRoutes);
 //         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
 //     });
 // }
+
+app.get('/', (req, res)=>{
+    res.send({hi: '1212'})
+});
+
+app.get('/.well-known/acme-challenge/:file', (req, res) =>{
+    console.log("printing content ======================================");
+    res.send(`DADr97Nje9iTTgEDw8y8YBJSkznmRryr5e1K0PACG0I.YWtMAGHQezdkbuF3jRq1jCKb5d-H7AY3V5dxcNAJGVk`);
+});
 
 const port = process.env.PORT || 4000;
 
--- routes/serverCertsApi.js
+++ routes/serverCertsApi.js
@@ -5,17 +5,11 @@ console.log("Inside sending Certs ==============================================
 
 router.get('/.well-known/acme-challenge/:file', (req, res) =>{
     console.log("printing content ======================================");
-    res.send(`Returned url/${req.params.file}`);
-//     if(req.params.content === 'FXXfXgptKmnRX9tL5fc8Wv6LI3ttRo2q2W6dVPsHwQg'){
-//         res.send('FXXfXgptKmnRX9tL5fc8Wv6LI3ttRo2q2W6dVPsHwQg.fEnpKypu6O07sLyde15azOh7TFJJVk-v9aaA0YjX7YY');
-//     }
-//     if (req.params.content === 'NBS8evhj85Ai_475NsnkCckPbU2_SmmvhC3eP00sFWk'){
-//         res.send('NBS8evhj85Ai_475NsnkCckPbU2_SmmvhC3eP00sFWk.fEnpKypu6O07sLyde15azOh7TFJJVk-v9aaA0YjX7YY');
-//     }
+    res.send('MvFzveRiERf_4LLOxg2My6mPEZ-5Vq6Pz9uH7XnqSfw.nSfKLLq8XbyisgpZJiMBbKnehGULN66Jl-lvTqMUn7Y')
 });
 
 router.get("/", (req, res)=>{
-    res.send("Inside Cert API");
+    res.send("Inside Cert API 1021");
 });
 
 module.exports = router;
